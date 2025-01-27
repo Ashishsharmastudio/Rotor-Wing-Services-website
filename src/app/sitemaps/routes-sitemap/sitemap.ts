87e7c725d6eefff2df0caac0b30afdd916ee9eef
ashish sharma <ashishsharmastudio@gmail.com>
2025-01-27 20:22:07 +0530

    comment out XML declaration and sitemap generation logic in various sitemap files

--- src/app/sitemaps/routes-sitemap/sitemap.ts
+++ src/app/sitemaps/routes-sitemap/sitemap.ts
@@ -1,31 +1,31 @@
-import { createClient } from "@/lib/contento";
-import { MetadataRoute } from "next";
+// import { createClient } from "@/lib/contento";
+// import { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
-    const client = createClient();
-    const limit = 100; // Set to a reasonable high limit
-    let response= await client.getContentByType({
-        contentType: "route_pages",
-        sortBy: "name",
-        sortDirection: "desc",
-        limit,
-    });
+//     const client = createClient();
+//     const limit = 100; // Set to a reasonable high limit
+//     let response= await client.getContentByType({
+//         contentType: "route_pages",
+//         sortBy: "name",
+//         sortDirection: "desc",
+//         limit,
+//     });
 
-    let content = [...response.content];
+//     let content = [...response.content];
 
-    while (response.nextPage) {
-        response = await response.nextPage();
-        content = content.concat(response.content);
-    }
+//     while (response.nextPage) {
+//         response = await response.nextPage();
+//         content = content.concat(response.content);
+//     }
 
-    // console.log("content--------------------", content);
-    const routeEntries: MetadataRoute.Sitemap = content.map(({slug, updated_at}) => ({
-        url: `https://www.jetlevel.com/${slug}`,
-        lastModified: new Date(updated_at)
-    }))
+//     // console.log("content--------------------", content);
+//     const routeEntries: MetadataRoute.Sitemap = content.map(({slug, updated_at}) => ({
+//         url: `https://www.jetlevel.com/${slug}`,
+//         lastModified: new Date(updated_at)
+//     }))
 
-    return [
-        ...routeEntries
-    ];
-}
+//     return [
+//         ...routeEntries
+//     ];
+// }
