cf88c70884b820c089fca07a1149f86c5866a125
ashish sharma <ashishsharmastudio@gmail.com>
2025-01-27 20:40:07 +0530

    Initial commit

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -6,7 +6,7 @@ import useEmblaCarousel, {
 } from "embla-carousel-react"
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+// import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
 type CarouselApi = UseEmblaCarouselType[1]
@@ -134,7 +134,7 @@ const Carousel = React.forwardRef<
           canScrollNext,
         }}
       >
-        <div
+        {/* <div
           ref={ref}
           onKeyDownCapture={handleKeyDown}
           className={cn("relative", className)}
@@ -143,7 +143,7 @@ const Carousel = React.forwardRef<
           {...props}
         >
           {children}
-        </div>
+        </div> */}
       </CarouselContext.Provider>
     )
   }
@@ -157,17 +157,18 @@ const CarouselContent = React.forwardRef<
   const { carouselRef, orientation } = useCarousel()
 
   return (
-    <div ref={carouselRef} className="overflow-hidden">
-      <div
-        ref={ref}
-        className={cn(
-          "flex",
-          orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
-          className
-        )}
-        {...props}
-      />
-    </div>
+    <div><h1>hi</h1></div>
+    // <div ref={carouselRef} className="overflow-hidden">
+    //   <div
+    //     ref={ref}
+    //     className={cn(
+    //       "flex",
+    //       orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
+    //       className
+    //     )}
+    //     {...props}
+    //   />
+    // </div>
   )
 })
 CarouselContent.displayName = "CarouselContent"
@@ -179,17 +180,18 @@ const CarouselItem = React.forwardRef<
   const { orientation } = useCarousel()
 
   return (
-    <div
-      ref={ref}
-      role="group"
-      aria-roledescription="slide"
-      className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
-        className
-      )}
-      {...props}
-    />
+    <div>s</div>
+    // <div
+    //   ref={ref}
+    //   role="group"
+    //   aria-roledescription="slide"
+    //   className={cn(
+    //     "min-w-0 shrink-0 grow-0 basis-full",
+    //     orientation === "horizontal" ? "pl-4" : "pt-4",
+    //     className
+    //   )}
+    //   {...props}
+    // />
   )
 })
 CarouselItem.displayName = "CarouselItem"
@@ -201,24 +203,25 @@ const CarouselPrevious = React.forwardRef<
   const { orientation, scrollPrev, canScrollPrev } = useCarousel()
 
   return (
-    <Button
-      ref={ref}
-      variant={variant}
-      size={size}
-      className={cn(
-        "absolute  h-8 w-8 rounded-full",
-        orientation === "horizontal"
-          ? "-left-12 top-1/2 -translate-y-1/2"
-          : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
-        className
-      )}
-      disabled={!canScrollPrev}
-      onClick={scrollPrev}
-      {...props}
-    >
-      <ArrowLeft className="h-4 w-4" />
-      <span className="sr-only">Previous slide</span>
-    </Button>
+    <Button>sis</Button>
+    // <Button
+    //   ref={ref}
+    //   variant={variant}
+    //   size={size}
+    //   className={cn(
+    //     "absolute  h-8 w-8 rounded-full",
+    //     orientation === "horizontal"
+    //       ? "-left-12 top-1/2 -translate-y-1/2"
+    //       : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
+    //     className
+    //   )}
+    //   disabled={!canScrollPrev}
+    //   onClick={scrollPrev}
+    //   {...props}
+    // >
+    //   <ArrowLeft className="h-4 w-4" />
+    //   <span className="sr-only">Previous slide</span>
+    // </Button>
   )
 })
 CarouselPrevious.displayName = "CarouselPrevious"
@@ -230,24 +233,25 @@ const CarouselNext = React.forwardRef<
   const { orientation, scrollNext, canScrollNext } = useCarousel()
 
   return (
-    <Button
-      ref={ref}
-      variant={variant}
-      size={size}
-      className={cn(
-        "absolute h-8 w-8 rounded-full",
-        orientation === "horizontal"
-          ? "-right-12 top-1/2 -translate-y-1/2"
-          : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
-        className
-      )}
-      disabled={!canScrollNext}
-      onClick={scrollNext}
-      {...props}
-    >
-      <ArrowRight className="h-4 w-4" />
-      <span className="sr-only">Next slide</span>
-    </Button>
+    // <Button
+    //   ref={ref}
+    //   variant={variant}
+    //   size={size}
+    //   className={cn(
+    //     "absolute h-8 w-8 rounded-full",
+    //     orientation === "horizontal"
+    //       ? "-right-12 top-1/2 -translate-y-1/2"
+    //       : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
+    //     className
+    //   )}
+    //   disabled={!canScrollNext}
+    //   onClick={scrollNext}
+    //   {...props}
+    // >
+    //   <ArrowRight className="h-4 w-4" />
+    //   <span className="sr-only">Next slide</span>
+    // </Button>
+    <Button>sis</Button>
   )
 })
 CarouselNext.displayName = "CarouselNext"
--- src/lib/contento.ts
+++ src/lib/contento.ts
@@ -1,15 +1,15 @@
-import { createContentoClient } from '@gocontento/client'
-import { ContentoClient } from '@gocontento/client/lib/client'
+// import { createContentoClient } from '@gocontento/client'
+// import { ContentoClient } from '@gocontento/client/lib/client'
 
-export function createClient(isPreview: boolean = false): ContentoClient {
+// export function createClient(isPreview: boolean = false): ContentoClient {
 
-  return createContentoClient({
-    apiURL: process.env.CONTENTO_API_URL ?? '',
-    apiKey: process.env.CONTENTO_API_KEY ?? '',
-    siteId: process.env.CONTENTO_SITE_ID ?? '',
-    isPreview: isPreview,
-  })
-}
+//   return createContentoClient({
+//     apiURL: process.env.CONTENTO_API_URL ?? '',
+//     apiKey: process.env.CONTENTO_API_KEY ?? '',
+//     siteId: process.env.CONTENTO_SITE_ID ?? '',
+//     isPreview: isPreview,
+//   })
+// }
 
 
 
